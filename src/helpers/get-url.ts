ba3b7a40d6a4a3b0d216e15575b7f33e46822dab
fsRoger <fsroger@outlook.com>
2024-07-26 19:08:22 -0300

    limpando testes

--- src/helpers/get-url.ts
+++
@@ -1,6 +0,0 @@
-// export function getUrl(path?: string) {
-//     const baseUrl = process.env.NEXTAUTH_URL || "";
-//     const normalizedPath =
-//       path && !path.startsWith("/") ? `/${path}` : path || "";
-//     return `${baseUrl}${normalizedPath}`;
-//   }
