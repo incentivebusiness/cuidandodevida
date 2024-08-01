ac82de119024680641c8c787a6945e0650fa2d62
Victor Lupi <victor.lupi@hotmail.com>
2024-08-01 19:39:17 -0300

    adicionando o meio de pagamento na página, e também, um link para o Portal do Cliente

---
+++ src/components/StripePricingTable.tsx
@@ -0,0 +1,23 @@
+import React, { useEffect } from "react";
+
+const StripePricingTable: React.FC = () => {
+  useEffect(() => {
+    if (window.Stripe) {
+      const stripe = window.Stripe(
+        "pk_live_51PfVFkRqFdcOn0glKZ4r2G3I9zPa7WyIAZ9QnHmEuQ7rYD9iFq4P1PnTHe2FAjq3O9fbcK1oHNLUJQHoeCap0Y1m000aJjbz43"
+      );
+      stripe.pricingTable.create({
+        pricingTableId: "prctbl_1PinyjRqFdcOn0glV2wjLqB9",
+        element: "#stripe-pricing-table",
+      });
+    }
+  }, []);
+
+  return React.createElement("stripe-pricing-table", {
+    "pricing-table-id": "prctbl_1PinyjRqFdcOn0glV2wjLqB9",
+    "publishable-key":
+      "pk_live_51PfVFkRqFdcOn0glKZ4r2G3I9zPa7WyIAZ9QnHmEuQ7rYD9iFq4P1PnTHe2FAjq3O9fbcK1oHNLUJQHoeCap0Y1m000aJjbz43",
+  });
+};
+
+export default StripePricingTable;
